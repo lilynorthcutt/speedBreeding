0ae7d05f179db6813832c5646fffff8ea30ab699
lilynorthcutt <northcuttlilya@gmail.com>
2024-06-28 22:18:45 -0400

    Added hypothesis test of basal branchesby variety

--- hypothesisTests.R
+++ hypothesisTests.R
@@ -104,6 +104,7 @@ ggplot(plot_data)+
 
 
 
+
 #######
 ### Leaf Number
 #######
@@ -177,7 +178,7 @@ ggplot(plot_data)+
 
 
 #######
-### Basal Branch Number
+### Basal Branch Number 1
 ######
 # Select necessary cols for height
 df <- summary_2023 %>% select(variety, experiment, avg_basal) %>% 
@@ -199,8 +200,76 @@ print(tukey_result)
 print(confint(tukey_result))
 
 
+######
+### Basal branch Number 2
+############
+# Select necessary cols for leaf Num
+df <- basal_branches %>% mutate(label_23 = variety) %>% 
+  merge(variety_key %>% select(name, gbs, label_23), by = 'label_23') %>%
+  select(-label_23) %>%  
+  mutate(basal_branches = as.numeric(basal_branches)) %>%  # change to feature_col here
+  filter(!is.na(basal_branches)) # change to feature_col here
 
-#######
+# Convert date to factor
+df$date <- factor(df$date)
+
+# Running for each individual 
+# Perform ANOVA for each plant variety
+anova_results <- list()
+emmeans_results <- list()
+plot_data <- data.frame()
+
+for (variety in unique(df$variety)) {
+  # Subset data for the current variety
+  variety_data <- df %>% filter(variety == !!variety)
+  
+  # Check if there are at least two levels of 'experiment' with data
+  if (length(unique(variety_data$experiment)) < 2) {
+    cat("Variety:", variety, "- Not enough data for comparison\n")
+    next
+  }
+  
+  # Perform ANOVA
+  aov_model <- aov(basal_branches ~ experiment  , data = variety_data)
+  anova_results[[variety]] <- summary(aov_model)
+  
+  # Perform Tukey's HSD test using emmeans package
+  emmeans_model <- emmeans(aov_model, ~ experiment)
+  emmeans_result <- pairs(emmeans_model, adjust = "tukey")
+  emmeans_results[[variety]] <- emmeans_result
+  
+  # Combine with confidence intervals
+  ci <- as.data.frame(confint(emmeans_result)) %>% 
+    mutate(name = variety_data$name[[1]])
+  
+  # Append to plot_data
+  plot_data <- bind_rows(plot_data, ci)
+  
+}
+
+# Print results
+for (variety in unique(df$variety)) {
+  cat("Variety:", variety, "\n")
+  print(anova_results[[variety]])
+  print(emmeans_results[[variety]])
+  cat("\n")
+}
+
+ggplot(plot_data)+
+  geom_point(aes(x = name, y = estimate))+
+  geom_errorbar(aes(name,ymin = lower.CL, ymax = upper.CL), width = 0.2)+
+  geom_hline(yintercept=0, linetype="dashed", color = "red")+
+  coord_flip()+
+  ylab('Difference in SB and C')+
+  xlab("")+
+  ggtitle("Tukeys HSD on Difference in Basal Branches")
+
+
+
+
+
+
+
 ### Leaf Width
 ######
 # Select necessary cols for height
@@ -227,6 +296,8 @@ print(confint(tukey_result))
 
 ### Add Flowers
 
+#####
+#####
 #####
 ### RATE OF CHANGE
 ################################################################################
